d48609aa52b53b21fef2e536cd9327900e2c3e6e
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-19 02:43:54 +0000

    feat: initialize fastify
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+import {config as dotenvConfig} from 'dotenv';
+import createFastify from 'fastify';
+
+if (process.env.NODE_ENV === 'development') {
+	dotenvConfig();
+}
+
+const app = createFastify({
+	trustProxy: true,
+	logger: process.env.NODE_ENV === 'development',
+});
+const serverPort = parseInt(process.env.PORT ?? '', 10) ?? 3000;
+
+app.listen({
+	port: serverPort,
+	host: '0.0.0.0',
+}, (err, address) => {
+	if (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+
+	app.log.info(`Server listening on ${address} with port ${serverPort}`);
+});
